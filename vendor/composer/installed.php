4c4ca406dd55eeb4622d5a063de3544df1b9b063
Rémy Leroy <remy.leroy77@hotmail.fr>
2025-01-19 21:01:52 +0100

    style+DataFixture+txtChoix

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '75675782bf97f0c53270ed1a29070a0cb32294a6',
+        'reference' => '4693214d81c4d6281d892bef216e63661d364713',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '75675782bf97f0c53270ed1a29070a0cb32294a6',
+            'reference' => '4693214d81c4d6281d892bef216e63661d364713',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
